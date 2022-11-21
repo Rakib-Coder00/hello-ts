918d4af0b8c7d9e9299f0f51eab8c4674e515236
Rakib Hassan <rhbappy456@gmail.com>
2022-11-22 00:42:31 +0600

    Object type

--- src/index.ts
+++ src/index.ts
@@ -75,8 +75,20 @@
 // }
 // calculateTax(100)
 
+
+
 //Object ==>
+let employee:{
+   readonly id:number,
+    name: string,
+    retire: (date:Date) => void
+} = {
+    id:1,
+    name: 'rakib',
+    retire: (date:Date)=>{
+        console.log(date)
+    }
+    }
 
-let employee = {id:1}
+// employee.id = 2
 
-employee.name = 'rakib'
